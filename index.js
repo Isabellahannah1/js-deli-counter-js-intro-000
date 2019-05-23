666470f5afeb9013d4d64ea2f3263182f1fbf774
Isabella Hannah <isabellahannah735@gmail.com>
2019-05-23 09:03:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,9 +23,10 @@ var line = [];
 
 function currentLine(katzDeli) {
   for (let i = 0; i < katzDeli.length; i++) {
-    line.push
+    line.push(` `+[i+1]+`. `  + katzDeli[i]);
   }
-if (katzDeli.length === 0) {
-  return "The line is currently empty.";
-}
+  if (katzDeli.length === 0) {
+    return "The line is currently empty.";
+  } else
+  return(`The line is currently:` + line);
 }
