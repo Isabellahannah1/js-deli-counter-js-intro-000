b92ad1710da4bfebf9f493febfdc5dc8726d5281
Isabella Hannah <isabellahannah735@gmail.com>
2019-05-23 08:58:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,9 @@ function nowServing(katzDeli) {
 var line = [];
 
 function currentLine(katzDeli) {
-  for (let i = 0; i < katzDeli.length; i++)
+  for (let i = 0; i < katzDeli.length; i++) {
+    line.push
+  }
 if (katzDeli.length === 0) {
   return "The line is currently empty.";
 }
