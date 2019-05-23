4419e21f76aa5c0cfe59f12f00a11b5ed4f381b2
Isabella Hannah <isabellahannah735@gmail.com>
2019-05-23 08:49:49 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,28 @@
+var katzDeli = []; // At the beginning of the day,
+//the deli is empty and is represented by an empty array
+//
+
+ function takeANumber(katzDeli, name) {
+  katzDeli.push(`${name}`);
+  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+}
+
+function nowServing(katzDeli) {
+  let i = 0;
+  while (i < katzDeli.length) {
+    i++;
+  }
+  if (katzDeli.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  else
+  return (`Currently serving ${katzDeli.shift()}.`);
+}
+
+var line = [];
+
+function currentLine(katzDeli) {
+if (katzDeli.length === 0) {
+  return "The line is currently empty.";
+}
+}
